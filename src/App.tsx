f1aaaa187246c32e969a6ffd2ca952381de86683
Arthur <arthur.nesterenko.dev@gmail.com>
2023-03-14 11:29:37 +0200

    Add ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import './assets/index.css';
 
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+});
+
+
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <ApolloProvider client={client}>
+        <h1>Hi there</h1>
+    </ApolloProvider>
 }
 
 export default App;
