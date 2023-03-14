90238e6a317ce1f923ae8727d60175a16903fa8a
Arthur <arthur.nesterenko.dev@gmail.com>
2023-03-14 14:26:17 +0200

    Prevent open auth screens for authorized user

--- src/layout/auth-layout.tsx
+++ src/layout/auth-layout.tsx
@@ -1,8 +1,20 @@
-import {Outlet} from "react-router-dom";
+import {
+    useLocation,
+    Navigate,
+    Outlet
+} from "react-router-dom";
+import {useAuth} from "../context/auth";
 
 
 const AuthLayout = () => {
 
+    const {isLoggedIn} = useAuth();
+    let location = useLocation();
+
+    if (isLoggedIn) {
+        const redirect = location.state?.from?.pathname ?? '/'
+        return <Navigate to={redirect} replace/>
+    }
 
     return <div className='h-screen flex flex-col items-center justify-center bg-[#f6f7f8]'>
         <Outlet/>
